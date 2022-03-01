95677ffcadcf42d241d0c7f5623dae136d603381
SzabolcsGergely <szabi@luxonis.com>
2022-03-02 00:10:06 +0200

    POC: improve spatial/temporal filtering by doing calculations in fp16 domain

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "456246cec6c17358f9eda61a41f45059c6ec7a2b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "efb5cdb98f2ce107cca6470cf4ddb43d691c47a5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
